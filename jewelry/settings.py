6e58201cd148a172db4b16722e0103e87eb899c6
Leena Prajapati <leena.prajapati46@gmail.co>
2025-06-18 22:30:32 +0530

    Updated phone/email/setting value

--- jewelry/settings.py
+++ jewelry/settings.py
@@ -32,7 +32,7 @@ EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT =  int(os.getenv("EMAIL_PORT"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == 'True'
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
